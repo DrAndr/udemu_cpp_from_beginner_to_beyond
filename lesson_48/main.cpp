b3c30a7166749f10a55c46006eb719114c26c4b0
DenDevCentury <denmag2020@gmail.com>
2024-02-16 00:27:05 +0200

    lesson_48 completed

--- lesson_48/main.cpp
+++ lesson_48/main.cpp
@@ -19,7 +19,6 @@ int main()
 
     cout << "char: " << sizeof(char) << endl;
     cout << "string: " << sizeof(string) << endl;
-    
 
     cout << "===========================" << endl;
 
@@ -28,8 +27,35 @@ int main()
     cout << "long double: " << sizeof(long double) << endl;
 
     cout << "===========================" << endl;
+    cout << "MIN_VALUES" << endl;
 
-    
+    cout << "char: " << CHAR_MIN << endl;
+    cout << "int: " << INT_MIN << endl;
+    cout << "short: " << SHRT_MIN << endl;
+    cout << "long: " << LONG_MIN << endl;
+    cout << "long long: " << LLONG_MIN << endl;
+
+    cout << "===========================" << endl;
+    cout << "MAX_VALUES" << endl;
+
+    cout << "char: " << CHAR_MAX << endl;
+    cout << "int: " << INT_MAX << endl;
+    cout << "short: " << SHRT_MAX << endl;
+    cout << "long: " << LONG_MAX << endl;
+    cout << "long long: " << LLONG_MAX << endl;
+
+    cout << "===========================" << endl;
+    cout << "Sizeof using variables names" << endl;
+
+    int age{21};
+    cout << "sizeof(age) is : " << sizeof(age) << endl;
+    cout << "or" << endl;
+    cout << "sizeof age is : " << sizeof age << endl;
+
+    double wage{22.25};
+    cout << "sizeof(wage) is : " << sizeof(wage) << endl;
+    cout << "or" << endl;
+    cout << "sizeof wage is : " << sizeof wage << endl;
 
     return 0;
 }
