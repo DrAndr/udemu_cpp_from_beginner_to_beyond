79d155011e2f35375bb4e98699657afd7e620521
YAndrey <nextdz@gmail.com>
2024-02-22 22:14:57 +0200

    lesson_59 homework

--- lesson_59__vectors/main.cpp
+++ lesson_59__vectors/main.cpp
@@ -1,6 +1,6 @@
 /**
  * Section 6
- * Lesson 57
+ * Lesson 59
  * Subject: vector
  */
 
@@ -30,5 +30,41 @@ int main()
     cout << "Fourth test_scores.at(3): " << test_scores.at(3) << endl;
     cout << "Fifth test_scores.at(4): " << test_scores.at(4) << endl;
 
+    cout << "\n=== home work ===" << endl;
+
+    vector<int> vector_1;
+    vector<int> vector_2;
+
+    cout << "\n=== vector_1 ===" << endl;
+    vector_1.push_back(10);
+    cout << "First vector_1.at(0): " << vector_1.at(0) << endl;
+    vector_1.push_back(20);
+    cout << "Second vector_1.at(1): " << vector_1.at(1) << endl;
+
+    cout << "\n=== vector_2 ===" << endl;
+    vector_2.push_back(100);
+    cout << "First vector_2.at(0): " << vector_2.at(0) << endl;
+    vector_2.push_back(200);
+    cout << "Second vector_2.at(1): " << vector_2.at(1) << endl;
+
+    vector<vector<int>> vector_2d;
+    vector_2d.push_back(vector_1);
+    vector_2d.push_back(vector_2);
+
+    cout << "\n=== vector_2d ===" << endl;
+    cout << "First vector_2d.at(0).at(0): " << vector_2d.at(0).at(0) << endl;
+    cout << "Second vector_2d.at(0).at(1): " << vector_2d.at(0).at(1) << endl;
+    cout << "Third vector_2d.at(1).at(0): " << vector_2d.at(1).at(0) << endl;
+    cout << "Fourth vector_2d.at(1).at(1): " << vector_2d.at(1).at(1) << endl;
+
+    cout << "\n=== vector_2d modified ===" << endl;
+    vector_2d.at(0).at(0) = 1000;
+    vector_2d.at(0).at(1) = 2000;
+    vector_2d.at(1).at(0) = 3000;
+    vector_2d.at(1).at(1) = 4000;
+    cout << "First vector_2d.at(0).at(0): " << vector_2d.at(0).at(0) << endl;
+    cout << "Second vector_2d.at(0).at(1): " << vector_2d.at(0).at(1) << endl;
+    cout << "Third vector_2d.at(1).at(0): " << vector_2d.at(1).at(0) << endl;
+    cout << "Fourth vector_2d.at(1).at(1): " << vector_2d.at(1).at(1) << endl;
     return 0;
 }
