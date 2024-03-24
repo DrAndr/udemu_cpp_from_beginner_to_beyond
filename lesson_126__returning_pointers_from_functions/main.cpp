e60bac0aa1defb71f232c9a8f33638c1dca708be
YAndrey <nextdz@gmail.com>
2024-03-24 21:02:45 +0200

    lesson_126 first example

--- lesson_126__returning_pointers_from_functions/main.cpp
+++ lesson_126__returning_pointers_from_functions/main.cpp
@@ -8,10 +8,24 @@
 
 using namespace std;
 
+int *largest_int(int *, int *);
+
 int main()
 {
+    int a{5};
+    int b{10};
+    int *result{};
 
-   
+    result = largest_int(&a, &b);
+    cout << "result: " << result << endl;   // 0x7ff7bc6604f4
+    cout << "*result: " << *result << endl; // 10
+
+    cout << "----------------------" << endl;
 
     return 0;
+}
+
+int *largest_int(int *num_1, int *num_2)
+{        // val      val     link    link
+    return *num_1 > *num_2 ? num_1 : num_2;
 }
