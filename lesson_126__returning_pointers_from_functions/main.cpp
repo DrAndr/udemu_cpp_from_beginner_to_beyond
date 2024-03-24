24062f4331048540b9b7c913440f012e9e998d67
YAndrey <nextdz@gmail.com>
2024-03-24 20:51:55 +0200

    lesson_126 init

---
+++ lesson_126__returning_pointers_from_functions/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * Section 12
+ * Subject: Pointers and references
+ * Lesson 126 [ Returning a pointer from a function ]
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   
+
+    return 0;
+}
