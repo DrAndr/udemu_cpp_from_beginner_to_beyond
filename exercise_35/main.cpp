a36f3199b690038e7dc86347b6ec309e1f92fc98
YAndrey <nextdz@gmail.com>
2024-03-28 00:55:18 +0200

    exercise_35 init

---
+++ exercise_35/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * Section 13
+ * Exercise 35
+ * Subject: OOP Classes and objects
+ *
+ * Problem title: Creating and Accessing Objects
+ *
+ *      We have already declared a C++ class named Dog  that is located in Dog.h
+ *      (we'll learn about header files later in this section).
+ *
+ *      In the test_dog  function create a Dog  object named spot  and,
+ *      using the . operator, set the name  attribute to the string "Spot"
+ *      and the age  attribute to 5 .
+ *
+ *      You can find my solution by clicking on the solution.txt file on the left pane.
+ *      But please make sure you give it a go yourself first, and only check the solution
+ *      if you really get stuck.
+ *
+ * Expected result:
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << endl;
+    return 0;
+}
