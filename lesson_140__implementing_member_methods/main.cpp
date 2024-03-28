4436552d4f4c5f8dbd19ac09bd9a57d3a118db1c
YAndrey <nextdz@gmail.com>
2024-03-29 01:13:10 +0200

    lesson_140 Implementing Member Methods

--- lesson_140__implementing_member_methods/main.cpp
+++ lesson_140__implementing_member_methods/main.cpp
@@ -1,9 +1,12 @@
 /**
  * Section 13
  * Subject: OOP Classes and objects
- * Lesson 140 [ Implementing Member Methods  ]
+ * Lesson 140 [ Implementing Member Methods ]
  *
  * Expected result:
+                Deposit OK.
+                Withdraw OK.
+                Not sufficient found.
  */
 
 #include <iostream>
@@ -37,6 +40,26 @@ bool Account::deposit(double dp)
 
 int main()
 {
+    Account frank_account;
+    frank_account.set_name("Frank's account");
+    frank_account.set_balance(1000.0);
+
+    if (frank_account.deposit(200.0))
+        cout << "Deposit OK." << endl;
+    else
+        cout << "Deposit not allowed." << endl;
+
+    if (frank_account.withdraw(500.0))
+        cout << "Withdraw OK." << endl;
+    else
+        cout << "Not sufficient found." << endl;
+
+    //...
+
+    if (frank_account.withdraw(1500.0))
+        cout << "Withdraw OK." << endl;
+    else
+        cout << "Not sufficient found." << endl;
 
     cout << endl;
 
