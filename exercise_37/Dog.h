6a8a64fcbfa7e1f4472379cbcf28a028eec5bed6
YAndrey <nextdz@gmail.com>
2024-03-31 01:44:10 +0200

    exercise_37 Added more public methods to the existing class

---
+++ exercise_37/Dog.h
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+
+class Dog
+{
+private:
+    string name;
+    int age{};
+
+public:
+    void set_name(std::string name)
+    {
+        this->name = name;
+        return;
+    };
+    std::string get_name() { return this->name; };
+
+    void set_age(int age)
+    {
+        this->age = age;
+        return;
+    };
+    int get_age() { return this->age; };
+
+    int get_human_years() { return this->age * 7; };
+
+    std::string speak() { return "Woof"; };
+
+    void print_sats() { cout << "Name: " << this->name << "; \nAge: " << this->age << endl; };
+};
