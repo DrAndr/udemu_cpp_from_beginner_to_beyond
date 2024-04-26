1b503c5d65f1506819b87dac3fe5430262b82de8
YAndrey <nextdz@gmail.com>
2024-04-26 23:36:22 +0300

    lesson_189

---
+++ lesson_189__:Polimorphism:Base_Class_reference/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * Section 15
+ * Subject: Polimorphism
+ * Lesson 189 [ Base Class reference]
+ *
+ * Expected result:
+ */
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+   
+
+    std::cout << "-= END =-" << std::endl;
+    return 0;
+}
